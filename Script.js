fc477ae948863fa63ddc5768369156c2a32850e2
Saniya khan <133801644+Saniyakhan7543@users.noreply.github.com>
2023-12-26 17:53:20 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+
+
+    function squareroot(){
+        var a,b;
+        a=Number(document.getElementById("first").value);
+        b=Math.sqrt(a);
+        document.getElementById("answer").innerHTML= b;
+        }
